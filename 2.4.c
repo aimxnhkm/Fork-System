44f5b8ac57c9ea6af0d7c41ef6970511ab61956b
aimxn <aimxn@aimxnhkm.com>
2020-10-24 08:40:39 +0000

    commit for 2.4

---
+++ 2.4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void childTask() {
+char name[50];
+
+printf("Please enter your name : ");
+fgets(name, 50, stdin);
+printf("Your name is : %s", name);
+exit(0);
+}
+
+void parentTask() {
+printf("Job is Done!\n");
+}
+
+int main(void) {
+int i;
+for(i = 0; i < 4; i++) {
+pid_t pid = fork();
+
+if(pid == 0) {
+childTask();
+exit(EXIT_SUCCESS);
+}
+
+else {
+wait(NULL);
+parentTask();
+}
+}
+
+return 0;
+}
