078ef0930e0a969fd5a80de6b54df8b8e6352a8e
aimxn <aimxn@aimxnhkm.com>
2020-10-24 15:57:38 +0000

    2.4 modified commit

--- 2.4.c
+++ 2.4.c
@@ -4,34 +4,36 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void childTask() {
-char name[50];
-
-printf("Please enter your name : ");
-fgets(name, 50, stdin);
-printf("Your name is : %s", name);
-exit(0);
+void childTask()
+{
+	char name[25];
+	printf("Sila isi nama anda : ");
+	scanf("%25s", name);
+	printf("Nama anda ialah %s\n", name);
+	exit(0);
 }
 
 void parentTask() {
-printf("Job is Done!\n");
+	printf("Job is Done!\n");
 }
 
 int main(void) {
-int i;
-for(i = 0; i < 4; i++) {
-pid_t pid = fork();
+	int i;
 
-if(pid == 0) {
-childTask();
-exit(EXIT_SUCCESS);
+	for(i = 0; i < 4; i++) {
+	pid_t pid = fork();
+
+	if(pid == 0) {
+		childTask();
+		exit(EXIT_SUCCESS);
+	}
+
+	else {
+		wait(NULL);
+
+	}
 }
-
-else {
-wait(NULL);
 parentTask();
-}
-}
 
 return 0;
 }
